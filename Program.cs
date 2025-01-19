e30fce835eeb7a82c1b594ab19084866cb0a6da1
dawkaka <yousiph77@gmail.com>
2025-01-19 22:22:52 +0000

    use apitoolit latest version

--- Program.cs
+++ Program.cs
@@ -1,66 +1,72 @@
 using ApiToolkit.Net;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Initialize the APItoolkit client
+
 var app = builder.Build();
 
 var config = new Config
 {
-    Debug = true,
-    ApiKey = "z6EYf5FMa3gzzNUfgKZsHjtN9GLETNaev7/v0LkNozFQ89nH"
+  Debug = true,
 };
-var client = await APIToolkit.NewClientAsync(config);
+var client = APIToolkit.NewClient(config);
 
 app.Use(async (context, next) =>
 {
-    var apiToolkit = new APIToolkit(next, client);
-    await apiToolkit.InvokeAsync(context);
+  var apiToolkit = new APIToolkit(next, client);
+  await apiToolkit.InvokeAsync(context);
 });
 
-app.MapGet("/don-net", async (context) =>
+app.MapGet("/", async (context) =>
 {
-    var observingHandlerOptions = new ATOptions
-    {
-        PathWildCard = "/posts/{id}/{name}",
-        RedactHeaders = ["User-Agent"],
-        RedactRequestBody = ["$.user.password"],
-        RedactResponseBody = ["$.user.data.email"]
-    };
-    using var httpClient = new HttpClient(client.APIToolkitObservingHandler(context, new ATOptions { PathWildCard = "/todos/{id}" }));
-    var request = new HttpRequestMessage(HttpMethod.Get, "https://jsonplaceholder.typicode.com/todos/1?vood=dooo");
-    client.ReportError(context, new Exception("ssssssssssss"));
-    // Add headers to the request
-    request.Headers.Add("User-Agent", "MyApp"); // Example header
+  var observingHandlerOptions = new ATOptions
+  {
+    PathWildCard = "/posts/{id}/{name}",
+    RedactHeaders = new List<string> { "User-Agent" },
+    RedactRequestBody = new List<string> { "$.user.password" },
+    RedactResponseBody = new List<string> { "$.user.data.email" }
+  };
+  using var httpClient = new HttpClient(client.APIToolkitObservingHandler(context, new ATOptions { PathWildCard = "/todos/{id}" }));
+  var request = new HttpRequestMessage(HttpMethod.Get, "https://jsonplaceholder.typicode.com/todos/1?vood=dooo");
+  Client.ReportError(context, new Exception("ssssssssssss"));
+  // Add headers to the request
+  request.Headers.Add("User-Agent", "MyApp"); // Example header
 
 
-    var response = await httpClient.SendAsync(request);
-    var body = await response.Content.ReadAsStringAsync();
-    Console.WriteLine(body);
-    await context.Response.WriteAsync("Hello World!");
+  var response = await httpClient.SendAsync(request);
+  var body = await response.Content.ReadAsStringAsync();
+  // Console.WriteLine(body);
+  await context.Response.WriteAsync(body);
 
 });
 
 app.MapGet("/error-tracking", async context =>
 {
-    try
-    {
-        // Attempt to open a non-existing file (just an example)
-        using (var fileStream = System.IO.File.OpenRead("nonexistingfile.txt"))
-        {
-            // File opened successfully, do something if needed
-        }
-        await context.Response.WriteAsync($"Hello, {context.Request.RouteValues["name"]}!");
-    }
-    catch (Exception ex)
-    {
-        client.ReportError(context, ex);
-        await context.Response.WriteAsync("Error reported!");
-    }
+  // Attempt to open a non-existing file (just an example)
+  using (var fileStream = System.IO.File.OpenRead("nonexistingfile.txt"))
+  {
+    // File opened successfully, do something if needed
+  }
+  await context.Response.WriteAsync($"Hello, {context.Request.RouteValues["name"]}!");
 });
 
 app.MapPost("/post-request", async context =>
 {
-    // Handle POST request here
-    await context.Response.WriteAsync("This is a POST request endpoint");
+  // Enable buffering to read the request body multiple times
+  context.Request.EnableBuffering();
+
+  // Read the request body
+  using (var reader = new StreamReader(context.Request.Body, leaveOpen: true))
+  {
+    string requestBody = await reader.ReadToEndAsync();
+    context.Request.Body.Position = 0; // Reset the stream position for further processing
+
+    // Return the request body as the response
+    await context.Response.WriteAsync($"Request Body: {requestBody}");
+  }
 });
 
 app.Run();
+
+
